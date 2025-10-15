4e99b0c6b6d141639e21709c82bd063b33e977d0
riyer4 <riyer4@ucsc.edu>
2025-10-15 15:57:01 +0000

    Added a title, a canvas, and a border (step 1 complete)
    Co-authored-by: Alexander-W-Mercer <Alexander-W-Mercer@users.noreply.github.com>
    Co-authored-by: Pl0the <Pl0the@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,25 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
+//import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
+//document.body.innerHTML = `
+//  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+//`;
+
+const h1 = document.createElement("h1");
+h1.textContent = "Sticker Sketchpad";
+document.body.append(h1);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+canvas.style.width = "256px";
+canvas.style.height = "256px";
+canvas.id = "sketch_canvas";
+document.body.append(canvas);
+
+const ctx = canvas.getContext("2d");
+if (ctx){
+  ctx.fillStyle = "green";
+  ctx.fillRect(10, 10, 150, 100);
+}
+
