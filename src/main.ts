2fe2e037270dcd80e2a440dfe24bb0c632bf6ffe
riyer4 <riyer4@ucsc.edu>
2025-10-17 22:30:27 +0000

    cleaned more of the code and added both the cursor tracking plus the clear button so the user is able to draw whatever and is able to clear it whenever they would like, also fixed the ctx possibly null issue (step 2 complete)

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,36 @@ canvas.id = "canvas";
 document.body.append(canvas);
 
 const ctx = canvas.getContext("2d");
-if (ctx) {
-  ctx.fillStyle = "green";
-  ctx.fillRect(10, 10, 150, 100);
-}
+
+const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", (e) => { //got from example
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (cursor.active && ctx) {
+    ctx.beginPath();
+    ctx.moveTo(cursor.x, cursor.y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+});
+
+const clearButton = document.createElement("button");
+clearButton.textContent = "Clear";
+document.body.append(clearButton);
+
+clearButton.addEventListener("click", () => {
+  if (ctx) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+});
